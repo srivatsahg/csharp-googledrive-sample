58fb41316e9da132f4403fd1edbad47576debcf2
H113585 <srivatsa.haridas@gmail.com>
2015-08-11 15:53:48 +0530

    Initial Version of the Google Drive application using the Oauth from Google API

---
+++ GoogleDriveFiles/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace GoogleDriveFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.colNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDetail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAuthenticate = new System.Windows.Forms.Button();
+            this.txtRefreshToken = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colNo,
+            this.colDetail});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(678, 372);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // colNo
+            // 
+            this.colNo.Text = "No";
+            // 
+            // colDetail
+            // 
+            this.colDetail.Text = "Details";
+            // 
+            // btnAuthenticate
+            // 
+            this.btnAuthenticate.Location = new System.Drawing.Point(274, 390);
+            this.btnAuthenticate.Name = "btnAuthenticate";
+            this.btnAuthenticate.Size = new System.Drawing.Size(161, 23);
+            this.btnAuthenticate.TabIndex = 1;
+            this.btnAuthenticate.Text = "Authenticate";
+            this.btnAuthenticate.UseVisualStyleBackColor = true;
+            this.btnAuthenticate.Click += new System.EventHandler(this.btnAuthenticate_Click);
+            // 
+            // txtRefreshToken
+            // 
+            this.txtRefreshToken.Location = new System.Drawing.Point(131, 419);
+            this.txtRefreshToken.Name = "txtRefreshToken";
+            this.txtRefreshToken.Size = new System.Drawing.Size(559, 20);
+            this.txtRefreshToken.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 425);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Refresh Token";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 488);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRefreshToken);
+            this.Controls.Add(this.btnAuthenticate);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Google Drive";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader colNo;
+        private System.Windows.Forms.ColumnHeader colDetail;
+        private System.Windows.Forms.Button btnAuthenticate;
+        private System.Windows.Forms.TextBox txtRefreshToken;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GoogleDriveFiles/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v2;
+using Google.Apis.Drive.v2.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace GoogleDriveFiles
+{
+    public partial class Form1 : Form
+    {
+        // Authenticate Oauth2
+        String CLIENT_ID = "137693713126-mimsg82hmi7nkrq49pa64mqhb5nmhrlk.apps.googleusercontent.com";
+        String CLIENT_SECRET = "-OmOEHgyoLNn6CUxYuew5_ZX";
+        string[] scopes = new string[] { DriveService.Scope.Drive,
+                                 DriveService.Scope.DriveFile};
+
+        private UserCredential credential = null;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAuthenticate_Click(object sender, EventArgs e)
+        {
+            // here is where we Request the user to give us access, or use the Refresh Token that was previously stored in %AppData%
+            credential = GoogleWebAuthorizationBroker.AuthorizeAsync(new ClientSecrets
+                                                                        {
+                                                                            ClientId = CLIENT_ID,
+                                                                            ClientSecret = CLIENT_SECRET
+                                                                        },
+                                                                         scopes,
+                                                                         Environment.UserName,
+                                                                         CancellationToken.None,
+                                                                         new FileDataStore("VATSAG.SiteVerification.Auth.Store")
+                                                                                            ).Result;
+
+            if (credential != null)
+            {
+                txtRefreshToken.Text = credential.Token.RefreshToken.ToString();
+
+                var service = new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "SiteVerification"
+                });
+
+                //Lists the directories on the Google Drive
+                //IList directories = GetFiles(service, "mimeType='application/vnd.google-apps.folder' and trashed=false");
+
+                //Lists all files on the Google Drive
+                IList directories = GetFiles(service, "trashed=false");
+
+                int i = 1;
+                foreach (var dir in directories)
+                {
+                    var item = dir as Google.Apis.Drive.v2.Data.File;
+                    if (item != null)
+                    {
+                        ListViewItem itemNo = new ListViewItem(i.ToString());
+                        itemNo.SubItems.Add(item.Title);
+                        listView1.Items.Add(itemNo);
+                        i++;
+                    }
+                }
+            }
+        }
+        /// 
+        /// List all of the files and directories for the current user.  
+        /// 
+        /// Documentation: https://developers.google.com/drive/v2/reference/files/list
+        /// Documentation Search: https://developers.google.com/drive/web/search-parameters
+        ///a Valid authenticated DriveService        
+        ///if Search is null will return all files        
+        /// 
+        public static IList GetFiles(DriveService service, string search)
+        {
+
+            IList Files = new List<File>();
+
+            try
+            {
+                //List all of the files and directories for the current user.  
+                // Documentation : https://developers.google.com/drive/v2/reference/files/list
+
+                FilesResource.ListRequest list = service.Files.List();
+                list.MaxResults = 1000;
+                if (search != null)
+                {
+                    list.Q = search;
+                }
+                FileList filesFeed = list.Execute();
+
+                //// Loop through until we arrive at an empty page
+                while (filesFeed.Items != null)
+                {
+                    // Adding each item  to the list.
+                    foreach (File item in filesFeed.Items)
+                    {
+                        Files.Add(item);
+                    }
+
+                    // We will know we are on the last page when the next page token is
+                    // null.
+                    // If this is the case, break.
+                    if (filesFeed.NextPageToken == null)
+                    {
+                        break;
+                    }
+
+                    // Prepare the next page of results
+                    list.PageToken = filesFeed.NextPageToken;
+
+                    // Execute and process the next page request
+                    filesFeed = list.Execute();
+                }
+            }
+            catch (Exception ex)
+            {
+                // In the event there is an error with the request.
+                Console.WriteLine(ex.Message);
+            }
+            return Files;
+        }
+
+    }
+}
